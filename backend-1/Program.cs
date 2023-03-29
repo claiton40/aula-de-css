e597857bf50bb1dfdad3a0f0533a17072b0f910b
Claiton Pereira De Siqueira <29321863885@info.local>
2023-03-29 15:36:06 -0300

    teste

---
+++ backend-1/Program.cs
@@ -0,0 +1,10 @@
+﻿// crie um programa para calcular o IMC de uma pessoa
+string nome = "CLaiton";
+float peso = 79f;
+float altura = 1.80f;
+float imc = peso / (altura * altura);
+Console.WriteLine ( "O IMC do " + nome + " e " + Math.Round(imc,2));
+
+
+
+
