9e0e1b3c7256a77ab1d4b0945e7b7430f68c9995
Claiton Pereira De Siqueira <29321863885@info.local>
2023-03-29 17:25:39 -0300

    nao consegui nem o primeiro exercicio

---
+++ backend-1/idade/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("insira o dia do nascimento: ");
+int dia = int.Parse (Console.ReadLine());
+Console.WriteLine("insira o mes do nascimento: ");
+int mes = int.Parse (Console.ReadLine());
+Console.WriteLine("insira o ano do nascimento: ");
+int ano = int.Parse (Console.ReadLine());
+Console.WriteLine("insira o hora do nascimento: ");
+int hora = int.Parse (Console.ReadLine());
+Console.WriteLine("insira os minutos: ");
+int minuto = int.Parse (Console.ReadLine());
+int resdias = (((2023-ano)*365));
+
+Console.WriteLine(resdias);
