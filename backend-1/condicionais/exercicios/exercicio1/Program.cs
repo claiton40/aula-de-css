8ddf16e5e8843308f21196622c1c1c094449045c
Claiton Pereira De Siqueira <29321863885@info.local>
2023-03-30 17:26:36 -0300

    eu seou burro pra caralho

---
+++ backend-1/condicionais/exercicios/exercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Faça um programa que o usuário informe o salário recebido e o total gasto. Deverá ser exibido na tela “Gastos dentro do orçamento” caso o valor gasto não ultrapasse o valor do salário e “Orçamento estourado” se o valor gasto ultrapassar o valor do salário.
+Console.WriteLine("Informe seu salario: ");
+float salario = float.Parse ( Console.ReadLine ()!);
+Console.WriteLine("Informe seu gasto total: ");
+float gasto = float.Parse ( Console.ReadLine ()!);
+if (gasto>salario) {
+    Console.WriteLine("Orçamento estourado");
+}
+else {
+     Console.WriteLine("Gastos dentro do orçamento");
+}
---
+++ backend-1/condicionais/exercicios/exercicio2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Desenvolva um programa que recebe do usuário, o placar de um jogo de futebol (os gols de cada time) e informa se o resultado foi um empate, se a vitória foi do primeiro time ou do segundo time.
+Console.WriteLine ("Informe o resultado do time 1");
+int time1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine ("Informe o resultado do time 2");
+int time2 = int.Parse(Console.ReadLine()!);
+if (time1==time2) {
+    "empate"
+
+}
