154b14cce8b8cc0ac46320d9aaaa365d9ddf6bba
Claiton Pereira de Siqueira <siqueiracla@crsc.sap.sp.gov.br>
2023-03-31 14:46:28 -0300

    ta andando os exercicios

--- backend-1/condicionais/exercicios/exercicio2/Program.cs
+++ backend-1/condicionais/exercicios/exercicio2/Program.cs
@@ -1,9 +1,21 @@
-﻿// Desenvolva um programa que recebe do usuário, o placar de um jogo de futebol (os gols de cada time) e informa se o resultado foi um empate, se a vitória foi do primeiro time ou do segundo time.
-Console.WriteLine ("Informe o resultado do time 1");
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Insira o resultado 1");
 int time1 = int.Parse(Console.ReadLine()!);
-Console.WriteLine ("Informe o resultado do time 2");
+Console.WriteLine("Insira o resultado 2");
 int time2 = int.Parse(Console.ReadLine()!);
-if (time1==time2) {
-    "empate"
 
+if (time1==time2) { 
+    Console.WriteLine("Empate");
 }
+
+else if (time1>time2) 
+{ 
+    Console.WriteLine("vitoria time1");
+    }    
+    
+
+else
+ {
+     Console.WriteLine("vitoria time2");
+}
+
---
+++ backend-1/condicionais/exercicios/exercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Escreva     um     programa que leia     as     medidas     dos     lados     de     um     triângulo     e    escreva    se    ele    é    Equilátero,    Isósceles    ou  Escaleno. 
+// Sendo    que:    
+// − Triângulo    Equilátero:    possui    os    3    lados    iguais.
+// − Triângulo    Isóscele:    possui    2    lados    iguais.
+// − Triângulo    Escaleno:    possui    3    lados    diferentes.
+
+Console.WriteLine("Insira a medida do lado1");
+float ld1 = float.Parse(Console.ReadLine()!);
+Console.WriteLine("Insira a medida do lado2");
+float ld2 = float.Parse(Console.ReadLine()!);
+Console.WriteLine("Insira a medida do lado3");
+float ld3 = float.Parse(Console.ReadLine()!);
+if (ld1 == ld2 && ld2 == ld3 && ld1 == ld3)
+{
+    Console.WriteLine("Triângulo Equilátero");
+}
+else if (ld1 != ld2 && ld2 != ld3 && ld1 != ld3)
+{
+    Console.WriteLine("Triângulo Escaleno");
+}
+else
+{
+    Console.WriteLine("Triângulo Isósceles");
+}
