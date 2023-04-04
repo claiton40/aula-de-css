b9e4019cd8b161fd6bbe3a4b34081f0849df1fd6
Claiton Pereira De Siqueira <29321863885@info.local>
2023-04-04 17:00:57 -0300

    buteco pronto

--- backend-1/switchcase/exerciciobuteco/Program.cs
+++ backend-1/switchcase/exerciciobuteco/Program.cs
@@ -9,7 +9,43 @@ Console.WriteLine($@"digite:
 1 para corote
 2 para jurupinga
 3 para etanol");
-int pinga = int.Parse(Console.ReadLine());
-
+char pinga = char.Parse(Console.ReadLine());
 Console.WriteLine ("Acompanha gelo? s ou n");
- 
+if (gelo == "S")
+{
+    Console.WriteLine ("com gelo");
+}
+
+switch (pinga)
+{
+    case '1':
+        Console.WriteLine($"voce escolheu corote");
+        break;
+    case '2':
+        Console.WriteLine($"voce escolheu jurupinga");
+        break;
+    case '3':
+        Console.WriteLine($"voce escolheu etanol");
+        break;
+default:
+        Console.WriteLine($"nao temos essa opcao");        
+        break;
+
+}
+
+// Console.WriteLine ("Acompanha gelo? s ou n");
+//  char gelo =  char.Parse(Console.ReadLine().ToUpper());
+// switch (gelo)
+// {
+//     case 'S':
+//         Console.WriteLine($"bebida acompanha gelo");
+//         break;
+//     case 'N':
+//         Console.WriteLine($"bebida sem gelo");
+//         break;
+
+// default:
+//         Console.WriteLine($"opcao invalida");        
+//         break;
+// }
+
