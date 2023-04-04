b11044bba8a97577779ae01be7179c4626f8012a
eu <eu@eu.com>
2023-04-04 13:56:55 -0300

    fazendo o buteco

--- backend-1/switchcase/exerciciobuteco/Program.cs
+++ backend-1/switchcase/exerciciobuteco/Program.cs
@@ -1,73 +1,15 @@
-﻿// //calculadora
+﻿// Crie um menu de opções de bebidas e peça para o usuário escolher uma das opções.
+// A depender da opção, exiba uma mensagem se gostaria de acrescentar gelo.
+// Se sim, exiba uma mensagem que a bebida em questão será com adicional de gelo, caso contrário, a bebida é sem gelo adicional.
+Console.WriteLine($@"Buteco do Claitinho
+Ambiente caseiro (bebe aqui, morre em casa)
 
-// //Algoritmo
+");
+Console.WriteLine($@"digite:
+1 para corote
+2 para jurupinga
+3 para etanol");
+int pinga = int.Parse(Console.ReadLine());
 
-// //1. informar a operacao 
-// //2. informar o primeiro numero
-// //3. informar o segundo numero
-// //4. processar os dados
-// //5. exibir o resultado
-
-
-// //entrada 
-
-// Console.WriteLine(@$"
-// -------------------------------------
-// | Programa de calculadora |   
-// |                                          | 
-// |   Informe a operação        |
-// |                                          |
-// |    (+) soma                        |
-// |    (-) subtração                  |
-// |    (*) multiplicação             |
-// |    (/) divisão                       |
-// --------------------------------------
-// ");
-
-// char operacao = char.Parse(Console.ReadLine());
-
-// Console.WriteLine($"Informe o primeiro número: ");
-// double num1 = double.Parse(Console.ReadLine());
-
-// Console.WriteLine($"Informe o segundo número: ");
-// double num2 = double.Parse(Console.ReadLine());
-
-// double resultado = 0;
-
-// //processamento
-// //saida
-
-// switch (operacao)
-// {
-//     case '+':
-//         resultado = (num1 + num2);
-//         Console.WriteLine($"O resultado da conta é igual á : {resultado}");
-//         break;
-//     case '-':
-//         resultado = (num1 - num2);
-//         Console.WriteLine($"O resultado da conta é igual á : {resultado}");
-//         break;
-//     case '*':
-//         resultado = (num1 * num2);
-//         Console.WriteLine($"O resultado da conta é igual á : {resultado}");
-//         break;
-//     case '/':
-//         resultado = (num1 / num2);
-//         Console.WriteLine($"O resultado da conta é igual á : {resultado}");
-//         break;
-//     default:
-//         Console.WriteLine($"Operação inválida, repita o processo!");        
-//         break;
-// }
-
-
-
-
-// ********************************************
-
-
-
-
-
-// adega do claitinho
-Console.WriteLine("Hello, World!");
+Console.WriteLine ("Acompanha gelo? s ou n");
+ 
