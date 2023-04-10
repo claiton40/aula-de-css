51fcb76119817d16158755b7f38fb5a189b304ce
Claiton Pereira De Siqueira <29321863885@info.local>
2023-04-10 16:37:55 -0300

    teste

--- backend-1/loops/exercicios 06042023/exercicio5/Program.cs
+++ backend-1/loops/exercicios 06042023/exercicio5/Program.cs
@@ -4,29 +4,44 @@
 // B.Total de Mulheres;      
 // C.Média de idade dos Homens;      
 // D.Média de idade das mulheres.
-int i =0;
-char f = char.Parse("f");
-char m = char.Parse("m");
-Console.WriteLine($"Insira o sexo da pessoa");
-char sexo = char.Parse(Console.ReadLine());
-// Console.WriteLine($"Insira a idade da pessoa");
-// int idade = int.Parse(Console.ReadLine());
-// Console.WriteLine($"Insira o peso da pessoa");
-// float peso = float.Parse(Console.ReadLine());
-
-for (i = 0; i < 10; i++);
-{
-Console.WriteLine($"Insira o sexo da pessoa");
+char sexo;
+int idade = 0;
+int totalMasculino =0;
+int totalFeminino =0;
+int totalIdadeFeminino=0;
+int totalIdadeMasculino=0;
+float mediaIdadeMasculino= 0;
+float mediaIdadeFeminino= 0;
+for (int i = 1; i <= 3; i++)
+{    
+Console.WriteLine(@$"Informe o sexo
+m - masculino
+f - feminino");
 sexo = char.Parse(Console.ReadLine());
-// Console.WriteLine($"Insira a idade da pessoa");
-// idade = int.Parse(Console.ReadLine());
-// Console.WriteLine($"Insira o peso da pessoa");
-// peso = float.Parse(Console.ReadLine());
+Console.WriteLine(@$"Informe o idade");
+idade = int.Parse(Console.ReadLine());
+if (sexo=='m')
+{
+totalMasculino++;
+totalIdadeMasculino=totalIdadeMasculino+idade;    
+}
+else if (sexo=='f')
+    {
+totalFeminino++;
+totalIdadeFeminino=totalIdadeFeminino+idade;
+    }
+else{
+    Console.WriteLine($"opcao invalida");
+    Console.WriteLine(@$"Informe o sexo
+m - masculino
+f - feminino");
+sexo = char.Parse(Console.ReadLine());
+Console.WriteLine(@$"Informe o idade");
+idade = int.Parse(Console.ReadLine());
+}
 }
 
-
-Console.WriteLine(i);
-Console.WriteLine(sexo-0);
-Console.WriteLine(f-sexo);
-
-
+Console.WriteLine(totalMasculino);
+Console.WriteLine(totalFeminino);
+Console.WriteLine(mediaIdadeMasculino);
+Console.WriteLine(mediaIdadeFeminino);
