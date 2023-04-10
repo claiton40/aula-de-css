2cb1d392e3fffd1367be3c1026a6723beb1e6108
Claiton Pereira de Siqueira <siqueiracla@crsc.sap.sp.gov.br>
2023-04-10 13:40:36 -0300

    ta dificil

---
+++ backend-1/loops/exercicios 06042023/exercicio5/Program.cs
@@ -0,0 +1,32 @@
+﻿// 5. Faça um programa que receba a idade, o peso e o sexo de 10 pessoas. Calcule e imprima:
+      
+// A.Total de Homens;      
+// B.Total de Mulheres;      
+// C.Média de idade dos Homens;      
+// D.Média de idade das mulheres.
+int i =0;
+char f = char.Parse("f");
+char m = char.Parse("m");
+Console.WriteLine($"Insira o sexo da pessoa");
+char sexo = char.Parse(Console.ReadLine());
+// Console.WriteLine($"Insira a idade da pessoa");
+// int idade = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Insira o peso da pessoa");
+// float peso = float.Parse(Console.ReadLine());
+
+for (i = 0; i < 10; i++);
+{
+Console.WriteLine($"Insira o sexo da pessoa");
+sexo = char.Parse(Console.ReadLine());
+// Console.WriteLine($"Insira a idade da pessoa");
+// idade = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Insira o peso da pessoa");
+// peso = float.Parse(Console.ReadLine());
+}
+
+
+Console.WriteLine(i);
+Console.WriteLine(sexo-0);
+Console.WriteLine(f-sexo);
+
+
