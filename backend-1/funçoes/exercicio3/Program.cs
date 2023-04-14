9a6696e44d84d785c2c5f056d64c685d3149df62
Claiton Pereira De Siqueira <29321863885@info.local>
2023-04-14 17:10:09 -0300

    posto de combustivel ok

--- backend-1/funçoes/exercicio3/Program.cs
+++ backend-1/funçoes/exercicio3/Program.cs
@@ -1,7 +1,7 @@
 ﻿// 1 - Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem que diga se
 // ela poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu).
 Console.WriteLine($"insira o ano de seu nascimento");
-int voto = int.Parse (Console.ReadKey());
+int voto = int.Parse (Console.ReadLine());
 int verificacao=2023-voto;
 if(verificacao<16)
 {
---
+++ backend-1/funçoes/exercicio4/Program.cs
@@ -0,0 +1,49 @@
+﻿// - Um posto está vendendo combustíveis com a seguinte tabela de descontos:
+// Álcool:
+// . até 20 litros, desconto de 3% por litro Álcool
+// . acima de 20 litros, desconto de 5% por litro
+// Gasolina:
+// . até 20 litros, desconto de 4% por litro Gasolina
+// . acima de 20 litros, desconto de 6% por litro
+// Escreva um algoritmo que leia o número de litros vendidos e o tipo de combustível (codificado
+// da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente
+// sabendo-se que o preço do litro da gasolina é R$ 5,30 e o preço do litro do álcool é R$ 4,90.
+// Dica: utilize switch case e funções/métodos para otimizar o algorítimo.
+
+double gasolina=5.30;
+double descontogasolina1=0.96;
+double descontogasolina2=0.95;
+double alcool=4.90;
+double descontoalcol1=0.97;
+double descontoalcol2=0.95;
+double valorpago=0;
+char combustivel =(' ');
+
+Console.WriteLine(@$"Escolha
+A - Alcool
+G - Gasolina");
+combustivel = char.Parse(Console.ReadLine().ToUpper());
+Console.WriteLine($"Insira os litros");
+double litro= double.Parse(Console.ReadLine());
+
+if(combustivel=='G')
+{
+if (litro<=20)
+{
+    valorpago=Math.Round((gasolina*litro)*descontogasolina1, 2);
+}
+else if(litro>20){
+    valorpago=Math.Round((gasolina*litro)*descontogasolina2, 2);
+}
+}
+else if((combustivel=='A'))
+{
+    if (litro<=20)
+{
+    valorpago=Math.Round((alcool*litro)*descontoalcol1, 2);
+}
+else if(litro>20){
+    valorpago=Math.Round((alcool*litro)*descontoalcol2, 2);
+}
+}
+Console.WriteLine(@$"O valor devido e R${valorpago},00");
