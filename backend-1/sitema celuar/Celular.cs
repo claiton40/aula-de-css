8b8b928cef44838fe1d1831ff0481477c61d7214
Claiton Pereira De Siqueira <29321863885@info.local>
2023-04-26 15:18:27 -0300

    sistema celular

---
+++ backend-1/sitema celuar/Celular.cs
@@ -0,0 +1,30 @@
+namespace sitema_celuar
+{
+    public class Celular
+// Crie uma Classe de um Celular, com as propriedades cor, modelo, tamanho, ligado(booleano).
+// Com os métodos, ligar, desligar, fazer ligação, enviar mensagem.
+// Só será possível executar tais métodos se o celular estiver ligado.
+// Envie o link do repositório como entrega desta atividade.
+    {
+     public string Cor;  
+     public string Modelo
+     public string Tamanho
+     public bool Ligado;
+
+public bool LigDesl()
+{
+if (Ligado=true)
+{
+    return bool.Parse("menu1");
+}
+else if (Ligado=false)
+{
+    return bool.Parse("menu2");
+}
+else
+{
+    bool.Parse("opcao invalida");
+}
+}
+}
+}
--- backend-1/sitema celuar/Program.cs
+++ backend-1/sitema celuar/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Crie uma Classe de um Celular, com as propriedades cor, modelo, tamanho, ligado(booleano).
+// Com os métodos, ligar, desligar, fazer ligação, enviar mensagem.
+// Só será possível executar tais métodos se o celular estiver ligado.
+// Envie o link do repositório como entrega desta atividade.
+
+using sitema_celuar;
+
+Console.WriteLine($@"o que vc deseja?
+|L|---> Ligar o Claitinhophone
+|D|---> Ligar o Claitinhophone");
+
+Celular novo = new Celular();
+
+novo.Ligado=Console.ReadLine();
+
