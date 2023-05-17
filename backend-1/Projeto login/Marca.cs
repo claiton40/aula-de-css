dc35b5982d82e165d37023921f4261706079304a
Claiton Pereira de Siqueira <siqueiracla@crsc.sap.sp.gov.br>
2023-05-17 12:15:03 -0300

    projeto de login parece que ta mais azeitado

--- backend-1/Projeto login/Marca.cs
+++ backend-1/Projeto login/Marca.cs
@@ -1,21 +1,55 @@
 namespace Projeto_login
 {
+    
     public class Marca
     {
-        private int Codigo;
-        private string NomeMarca;
-        // private DateTime Data;
+    
+        public int Codigo = 222;
+        public string NomeMarca= "Pakalolo";
+        public DateTime Data = DateTime.UtcNow;
 
-        Marca m = new Marca();
-        List<Marca> marcas = new List<Marca>();
-        m.Codigo=
-
+  List<Marca> marca = new List<Marca>();
+  Marca m = new Marca();
         public string Cadastrar() 
         {
-            return "teste";
+        //    falta fazer o loop de rpetição para por mais intens de forma dinamica
+
+            
+            marca.Add(m);
+            // Console.WriteLine($"Insira o Codigo da Marca{m.Codigo}");
+            // // m.Codigo = int.Parse(Console.ReadLine());
+            // Console.WriteLine($"Insira o nome da Marca {m.NomeMarca}");
+            // // m.NomeMarca= Console.ReadLine();
+            // Console.WriteLine($"Data do cadastro{m.Data}");
+                       
+            return $"Marca {m.marca} inserida com sucesso em {m.Data} ";
         }
-        
-        
+        public string Deletar() 
+        {
+            //    falta fazer o loop de rpetição para remover mais intens de forma dinamica
+        // verificar como fzer o remove dinamico depois de testar
+        marca.Remove(m);
+
+          
+          
+    
+            
+            return $"Marca X removida com sucesso em {m.Data}.";
+        }
+
+    public void Listar()
+     {
+        foreach (var m in marca)
+        {
+            Console.WriteLine(@$"
+            Codigo: {m.Codigo}
+            Maraa: {m.marca}
+            Data do cadastro: {m.Data}");
+            // preciso ver essa coisa da data pq a cada listagem ele vai dar data atual
+            
+        }        
+     }
+
     }
    
 }
--- backend-1/Projeto login/Usuario.cs
+++ backend-1/Projeto login/Usuario.cs
@@ -2,6 +2,18 @@ namespace Projeto_login
 {
     public class Usuario
     {
+        private int Codigo;
+        private string Nome;
+        private string Email;
+        private string Senha;
+
+        private DateTime DataDeCadastro;
+
+        public string Cadastrar()
+        // pq ta dando esse erro de merda?
+        {
+            return "teste";
+        }
         
     }
 }
