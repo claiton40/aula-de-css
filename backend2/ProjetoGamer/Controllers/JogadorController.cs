3e80b2ccc151dc1210d734fbdce85cc360f7a1ef
Claiton Pereira De Siqueira <29321863885@info.local>
2023-06-06 17:17:03 -0300

    listar e excluir jogador

--- backend2/ProjetoGamer/Controllers/JogadorController.cs
+++ backend2/ProjetoGamer/Controllers/JogadorController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 using ProjetoGamer.Infra;
+using ProjetoGamer.Models;
 
 namespace ProjetoGamer.Controllers
 {
@@ -23,6 +24,38 @@ namespace ProjetoGamer.Controllers
             ViewBag.Equipe = j.Equipe.ToList();
             //ViewBag e uma classe do framework para mostrar a lista na View
             return View();
+        }
+
+        [Route("Cadastrar")]
+
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            Jogador novoJogador = new Jogador();
+
+            novoJogador.Nome = form["Nome"].ToString();
+            novoJogador.Email = form["Email"].ToString();
+            novoJogador.IdEquipe = int.Parse(form["IdEquipe"].ToString());
+
+            j.Jogador.Add(novoJogador);
+            j.SaveChanges();
+            return LocalRedirect("~/Jogador/Listar");
+        }
+
+            [Route("Excluir/(id)")]
+        public IActionResult Excluir(int id)
+        {
+            Jogador jog = j.Jogador.First (jog => jog.IdJogador == id);
+             j.Jogador.Remove(jog);
+            j.SaveChanges();
+            return LocalRedirect("~/Jogador/Listar");
+        }
+
+        [Route("Editar/(id)")]
+        public IActionResult Editar(int id)
+        {
+            Jogador jog = j.Jogador.First (jog => jog.IdJogador == id);;
+            ViewBag.Jogador = jog;
+            return View("Edit");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
