09f76d9e29d4585433649a760abad344cea2fb78
Claiton Pereira De Siqueira <29321863885@info.local>
2023-09-01 17:12:29 -0300

    continuacao API 6 ta osso

--- segundo semestre/sprint2/API/WepApi.tarde/Controllers/UsuarioController.cs
+++ segundo semestre/sprint2/API/WepApi.tarde/Controllers/UsuarioController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using WepApi.tarde.Domains;
 using WepApi.tarde.Interfaces;
 using WepApi.tarde.Repositories;
@@ -18,17 +21,56 @@ namespace WepApi.tarde.Controllers
         }
         [HttpPost]
 
-        public IActionResult Login (string Email, string Senha)
+        public IActionResult Login(UsuarioDomain Usuario)
         {
-            UsuarioDomain UsuarioBuscado = _usuarioRepository.Login(Email, Senha);
+            UsuarioDomain UsuarioBuscado = _usuarioRepository.Login(Usuario.Email, Usuario.Senha);
             try
             {
-                if (UsuarioBuscado==null)
+                if (UsuarioBuscado == null)
                 {
                     return NotFound("Usuario nao encontrado, Email ou senha invalido");
                 }
+                //caso encontre o usuario buscado, prossegue para criacao do token
 
-                return Ok(UsuarioBuscado);
+                //dados que serao fornecidos no token(payload)
+                var claims = new[]
+                {
+                    //formato de cada clain. Tipo e valor delas
+                    new Claim(JwtRegisteredClaimNames.Jti, UsuarioBuscado.IdUsuario.ToString()),
+
+                    new Claim(JwtRegisteredClaimNames.Email, UsuarioBuscado.Email),
+
+                    new Claim(ClaimTypes.Role, UsuarioBuscado.Permissao),
+
+                    new Claim("Claim Personalizada", "Valor Personalizado")
+                };
+
+                //definir de acesso ao token
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("filmes-chave-autenticacao-webapi"));
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken
+                (
+                    issuer: "webapi.filmes.tarde",
+
+                    audience: "webapi.filmes.tarde",
+
+                    claims: claims,
+
+                    expires: DateTime.Now.AddMinutes(5),
+
+                    signingCredentials: creds
+                );
+                //retronar ot tokem criado
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            };
+
+               // return Ok(UsuarioBuscado);
             }
             catch (Exception ERRO)
             {
--- segundo semestre/sprint2/API/WepApi.tarde/Program.cs
+++ segundo semestre/sprint2/API/WepApi.tarde/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 //Adiciona o servico de controllers
 builder.Services.AddControllers();
+//adicona servico de autenticacao jwt bearer
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultChallengeScheme = "JwtBearer";
+    options.DefaultAuthenticateScheme = "JwtBearer";
+})
+
+//define os parametros de validacoa do token
+.AddJwtBearer(options => { });
+    
 var app = builder.Build();
 
 //mapeia os controllers
