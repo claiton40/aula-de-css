d22697a7dc71a100ee48f43ce366885e94c99b45
Claiton Pereira De Siqueira <29321863885@info.local>
2023-11-08 17:10:32 -0300

    testando o app de commit versao visual

--- segundo semestre/sprint 3/React/Element_+/eventplus/src/Components/Footer/Footer.jsx
+++ segundo semestre/sprint 3/React/Element_+/eventplus/src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = ({textRigths = "Escola do Claitinho"}) => {
+const Footer = ({textRigths = "Escola do Prof Claitinho"}) => {
     return (
         <footer className='footer-page'>
             <p className='footer-page__rights'>{textRigths}</p>
