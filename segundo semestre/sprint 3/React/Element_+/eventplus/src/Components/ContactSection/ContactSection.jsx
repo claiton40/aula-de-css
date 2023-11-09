cdc49f2d79d5c122e147da50826bbeede93b1d49
ciprila <pri@pri.com>
2023-11-09 13:54:33 -0300

    TESTANDO O GUI

--- segundo semestre/sprint 3/React/Element_+/eventplus/src/Components/ContactSection/ContactSection.jsx
+++ segundo semestre/sprint 3/React/Element_+/eventplus/src/Components/ContactSection/ContactSection.jsx
@@ -5,14 +5,25 @@ import Title from '../../Components/Title/Title';
 
 const ContactSection = () => {
     return (
-       <section className='contato'>
-        <Title titleText={"Contato"}/>
-        <div className='contato__endereco-box'>
-            <img src={contatoMap} alt="" 
-            className='contato__img-map'/>
+        <section className='contato'>
+        <Title titleText={"Contato"} />
 
+        <div className="contato__endereco-box">
+            <img 
+                src={contatoMap} 
+                alt="Imagem ilustrativa de um mapa"
+                className='contato__img-map'
+            />
+            <p>
+                Rua Niterói, 180 - Centro <br />
+                São Caetano do Sul - SP <br />
+                <a 
+                    href="tel:+551142252000"
+                    className='contato__telefone'
+                >(11) 4225-2000</a>
+            </p>
         </div>
-       </section>
+    </section>
     );
 };
 
